15bd15bc9dcecb051bc887319f060a62ebf006b9
Christian Brolin <33978252+1chb@users.noreply.github.com>
2021-04-02 09:25:38 +0200

    Add FunEqOrd module: Instances for comparing some types of functions.
    For equality:
    :: (Bounded a, Enum a, Eq b) => a -> b
    
    For ordering:
    :: (Bounded a, Enum a, Ord b) => a -> b
    
    Some examples:
    > ghci
    λ> :load FunEqOrd.hs
    [1 of 1] Compiling FunEqOrd         ( FunEqOrd.hs, interpreted )
    Ok, one module loaded.
    
    -- Comparing: Bool -> Bool
    λ> not == id
    False
    λ> compare not id
    GT
    
    -- Comparing: Int -> Bool
    λ> compare even (odd :: Int -> Bool)
    GT
    λ> minBound :: Int
    -2147483648
    
    -- Comparing: Int -> Int
    λ> (+1) == ((*2) :: Int -> Int)
    False
    
    -- Comparing: Bool -> Bool -> Bool
    λ> (&&) == (||)
    False
    λ> nand a b = not (a && b)
    λ> xor a b = let c = nand a b in nand (nand a c) (nand b c)
    λ> compare xor (/=)
    EQ
    
    -- Comparing: Int -> Int -> Int
    λ> mod == (div :: Int -> Int -> Int)
    False
    
    -- Not so fast:
    λ> and24 a b c d e f g h i j k l m n o p q r s t = and [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t]
    λ> and24 == and24
    True
    
    -- Too slow:
    λ> (+0) == ((*1) :: Int -> Int)
    ^CInterrupted.
    
    -- But this works:
    λ> import Data.Int
    λ> minBound :: Int16
    -32768
    λ> compare (\x -> x+x) ((*2) :: Int16 -> Int16)
    EQ
    λ> compare (\x -> if x==maxBound then minBound else x+x) ((*2) :: Int16 -> Int16)
    LT
    
    --Weird:
    λ> (\not -> not == not) == not
    False
    λ> :type \x -> (\x -> x == x) == x
    \x -> (\x -> x == x) == x
      :: (Bounded a, Enum a, Eq a) => (a -> Bool) -> Bool

---
+++ FunEqOrd.hs
@@ -0,0 +1,8 @@
+module FunEqOrd where
+
+instance (Bounded a, Enum a, Eq b) => Eq (a -> b) where
+  f == g = all (\x -> f x == g x) [minBound..]
+
+instance (Bounded a, Enum a, Ord b) => Ord (a -> b) where
+  compare f g = foldMap (\x -> compare (f x) (g x)) [minBound..]
+
