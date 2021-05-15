5e29a3cc5c6144b40b711d7f63e0709f43263f30
Christian Brolin <chb@nx-mate.localdomain>
2021-05-15 13:34:34 +0200

    Generalize MFAcc from String to a.

--- MFAcc.hs
+++ MFAcc.hs
@@ -4,22 +4,22 @@ module MFAcc where
 import Data.List.NonEmpty (NonEmpty((:|)), (<|))
 import System.Environment (getArgs)
 
-newtype MFAcc = MFAcc { un :: String -> String }
+newtype MFAcc a = MFAcc { un :: a -> a }
 
-instance Semigroup MFAcc where
+instance Semigroup (MFAcc a) where
   MFAcc f <> MFAcc g = MFAcc (f . g)
 
-instance Monoid MFAcc where
+instance Monoid (MFAcc a) where
   mempty = MFAcc id
 
-instance Show MFAcc where
-  showsPrec k (MFAcc f) = showParen (k>10) $ ("MFAcc (" ++) . shows (f "") . ("++)"++)
+instance (Monoid a, Show a) => Show (MFAcc a) where
+  showsPrec k (MFAcc f) = showParen (k>10) $ ("MFAcc (" ++) . shows (f mempty) . ("<>)"++)
 
 main :: IO ()
 main = do
   [n] <- getArgs
   let acc = mconcat $ map (MFAcc . shows) [1..read n]
   print $ length $ un acc "."
-  print $ Just (mempty :: MFAcc)
-  print (mempty :: MFAcc, mempty :: MFAcc)
+  print $ Just (mempty :: MFAcc ())
+  print (mempty :: MFAcc Ordering, mempty :: MFAcc (Maybe String))
   print $ MFAcc ("APA"++) <| MFAcc (shows "BEPA") :| []
