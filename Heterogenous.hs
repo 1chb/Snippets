a62150ee187bef63ec4cd4147718aa25b344b017
Christian Brolin <brolin@gmail.com>
2021-05-20 13:13:00 +0200

    Heterogenous wrapper with Eq, Ord, and Typeable.

--- Heterogenous.hs
+++ Heterogenous.hs
@@ -1,13 +1,22 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE StandaloneDeriving #-}
+import Data.List (sort)
+import Data.Typeable (Typeable, cast, typeOf)
 
 class C a where
   m :: a -> Bool
   n :: Bool -> a
 
-data D = forall a. (C a, Show a) => D a
+data D = forall a. (C a, Eq a, Ord a, Show a, Typeable a) => D a
 
 deriving instance Show D
+deriving instance Typeable D
+
+instance Eq D where
+  D a == D b = Just a == cast b
+
+instance Ord D where
+  compare (D a) (D b) = compare (typeOf a) (typeOf b) <> compare (Just a) (cast b)
 
 instance C () where
   m = const True
@@ -37,7 +46,8 @@ main = do
   let d4 = D (4 :: Int)
   let l = [D (), D (2 :: Int), D (3 :: Int), D (Just ()), d4, d3, D d3, D d4, D (D d3), D (Just d3)]
   print l
+  print $ sort l
   print $ f <$> l
   print $ f d4
-  print $ D d4
+  print (d4 == d4, D d4 == D (D d4), D (D d3) == D (D d4))
   print $ f . n <$> [False, True]
