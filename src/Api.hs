52ab39c9b354876d79c13049817d16217a4fcc35
Christian Brolin <christian.brolin@carboncloud.com>
2025-01-16 17:03:20 +0100

    Return header.

--- src/Api.hs
+++ src/Api.hs
@@ -2,14 +2,27 @@
 
 module Api (api, server) where
 
-import Servant (Get, PlainText, Proxy (..), Server, (:>))
+import Data.Maybe (fromMaybe)
+import Servant (Get, Header, Headers (..), JSON, PlainText, Proxy (..), Server, addHeader, (:<|>) (..), (:>))
 
 -- Define the API type
-type API = "hello" :> Get '[PlainText] String
+type API =
+  Get '[PlainText] String
+    :<|> "hello"
+      :> ( Header "User-Agent" String :> Get '[PlainText] String
+             :<|> "there" :> Get '[PlainText] String
+         )
+    :<|> "users" :> Get '[JSON] (Headers '[Header "User-Count" Integer] [String])
 
 -- Implement the server
 server :: Server API
-server = return "Hello, world!"
+server =
+  return "tack"
+    :<|> fmap ("Hello, " <>)
+      <$> ( (return . fromMaybe "???")
+              :<|> return "there!"
+          )
+    :<|> return (addHeader 2 ["X", "Y"])
 
 -- Create a Proxy for the API
 api :: Proxy API
