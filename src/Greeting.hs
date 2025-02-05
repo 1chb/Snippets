f840f68545c1a075d2e626abeaca0e36b642d543
Christian Brolin <christian.brolin@carboncloud.com>
2025-02-05 08:54:51 +0100

    Disable Add greeting button if no input (empty).

--- src/Greeting.hs
+++ src/Greeting.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Greeting (Form, table, form, get, add) where
+module Greeting (Form, get, add) where
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Text (Text)
 import Data.Text qualified as T
 import Database qualified as DB
 import GHC.Generics (Generic)
-import Lucid (Html, action_, body_, form_, h1_, head_, html_, input_, method_, name_, placeholder_, table_, td_, title_, toHtml, tr_, type_, value_)
+import Lucid (Html, action_, body_, button_, disabled_, form_, h1_, head_, html_, id_, input_, method_, name_, oninput_, onsubmit_, script_, table_, td_, title_, toHtml, tr_, type_)
 import Web.FormUrlEncoded (FromForm (..), parseUnique)
 
 data Form where
@@ -21,10 +21,28 @@ instance FromForm Form where
 table :: [String] -> Html ()
 table gs = table_ $ mapM_ (tr_ . td_ . toHtml) gs
 
-form :: Html ()
-form = form_ [method_ "post", action_ "/add-greeting"] $ do
-  input_ [type_ "text", name_ "greeting", placeholder_ "Enter new greeting"]
-  input_ [type_ "submit", value_ "Add Greeting"]
+input :: Html ()
+input = form_ [method_ "post", action_ "/add-greeting", onsubmit_ "return validateForm()"] $ do
+  input_ [type_ "text", id_ "greetingInput", name_ "greeting", oninput_ "validateInput()"]
+  button_ [type_ "submit", id_ "submitButton", disabled_ "disabled"] "Add Greeting"
+  script_
+    "function validateInput() {\
+    \  var input = document.getElementById('greetingInput').value;\
+    \  var button = document.getElementById('submitButton');\
+    \  if (input.trim() === '') {\
+    \    button.disabled = true;\
+    \  } else {\
+    \    button.disabled = false;\
+    \  }\
+    \}\
+    \function validateForm() {\
+    \  var input = document.getElementById('greetingInput').value;\
+    \  if (input.trim() === '') {\
+    \    alert('Greeting cannot be empty!');\
+    \    return false;\
+    \  }\
+    \  return true;\
+    \}"
 
 get :: (MonadIO m) => DB.Environment -> m (Html ())
 get env = do
@@ -34,7 +52,7 @@ get env = do
     body_ $ do
       h1_ "List of Greetings"
       table greetings
-      form
+      input
 
 add :: (MonadIO m) => DB.Environment -> Form -> m ()
 add env (Form newG) = do
