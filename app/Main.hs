f1975d649b117e245952dfbf73bf592d24dff530
Christian Brolin <christian.brolin@carboncloud.com>
2025-02-06 19:32:26 +0100

    Serve on https port 443.

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,8 @@ module Main where
 import Api (api, server)
 import Database qualified (connectAndMigrate)
 import MyLib qualified (someFunc)
-import Network.Wai.Handler.Warp qualified as Wai (run)
+import Network.Wai.Handler.Warp qualified as Wai (defaultSettings, setPort)
+import Network.Wai.Handler.WarpTLS (runTLS, tlsSettings)
 import Servant qualified (serve)
 
 main :: IO ()
@@ -11,5 +12,10 @@ main = do
   putStrLn "Hello, Haskell!"
   MyLib.someFunc
   -- Run the server
-  putStrLn "Starting server on port 8080..."
-  Database.connectAndMigrate >>= Wai.run 8080 . Servant.serve api . server
+  let port = 443
+  putStrLn $ "Starting server: " ++ show port
+  let tlsConfig = tlsSettings "/etc/letsencrypt/live/wv2.hopto.org/fullchain.pem" "/etc/letsencrypt/live/wv2.hopto.org/privkey.pem"
+  Database.connectAndMigrate
+    >>= runTLS tlsConfig (Wai.setPort port Wai.defaultSettings)
+      . Servant.serve api
+      . server
