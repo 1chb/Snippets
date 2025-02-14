bc2cef9518f6e2fe49ea9baa5bc750352438c75b
Christian Brolin <christian.brolin@carboncloud.com>
2025-02-14 14:50:14 +0100

    Add time in the logger callback.

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,14 +1,33 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module Logger (setup) where
 
+import Control.Monad qualified as Control
+import Control.Monad.Logger (fromLogStr, toLogStr)
+import Data.ByteString qualified as BS
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format (defaultTimeLocale, formatTime)
 import Network.Wai (Middleware)
-import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Network.Wai.Middleware.RequestLogger
+  ( Callback,
+    Destination (Callback, Handle, Logger),
+    RequestLoggerSettings (autoFlush),
+    defaultRequestLoggerSettings,
+    destination,
+    logStdoutDev,
+    mkRequestLogger,
+  )
 import System.IO (hFlush, stdout)
+import System.Log.FastLogger.LoggerSet (flushLogStr, pushLogStr)
 
 setup :: IO Middleware
-setup = do
-  pure $ timestampLogger . logStdoutDev
+setup =
+  if True
+    then do
+      let settings = defaultRequestLoggerSettings
+      mkRequestLogger settings {destination = Callback $ timestampCallback settings}
+    else do
+      pure $ timestampLogger . logStdoutDev
 
 timestampLogger :: Middleware
 timestampLogger app request sendResponse = do
@@ -19,3 +38,17 @@ timestampLogger app request sendResponse = do
     putStrLn $ "[" ++ timestamp ++ "] Response sent"
     hFlush stdout
     sendResponse response
+
+timestampCallback :: RequestLoggerSettings -> Callback
+timestampCallback requestLoggerSettings logStr = do
+  currentTime <- getCurrentTime
+  let timestamp = formatTime defaultTimeLocale "[%Y-%m-%d %H:%M:%S] " currentTime
+  callback $ toLogStr timestamp <> logStr
+  Control.when requestLoggerSettings.autoFlush flush
+  where
+    (callback, flush) =
+      -- This code was copied from mkRequestLogger
+      case destination requestLoggerSettings of
+        Handle h -> (BS.hPutStr h . fromLogStr, hFlush h)
+        Logger l -> (pushLogStr l, flushLogStr l)
+        Callback c -> (c, return ())
