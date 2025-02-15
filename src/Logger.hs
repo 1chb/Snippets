53053caa9bfae29a04654ca31b9c8926e600f17c
Christian Brolin <christian.brolin@carboncloud.com>
2025-02-15 17:03:23 +0100

    Log client IP address along with timestamp. Remove old timestamp logger.

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,39 +1,24 @@
-{-# LANGUAGE OverloadedRecordDot #-}
-
 module Logger (setup) where
 
 import Control.Monad qualified as Control
-import Control.Monad.Logger (fromLogStr, toLogStr)
-import Data.ByteString qualified as BS
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format (defaultTimeLocale, formatTime)
-import Network.Wai (Middleware)
-import Network.Wai.Middleware.RequestLogger
-  ( Callback,
-    Destination (Callback, Handle, Logger),
-    RequestLoggerSettings (autoFlush),
-    defaultRequestLoggerSettings,
-    destination,
-    mkRequestLogger,
-  )
-import System.IO (hFlush)
-import System.Log.FastLogger.LoggerSet (flushLogStr, pushLogStr)
+import Network.Wai (Middleware, Request (remoteHost, requestHeaders))
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import System.Console.ANSI (Color (..), ColorIntensity (Vivid), ConsoleLayer (..), SGR (SetColor, SetDefaultColor), setSGRCode)
 
 setup :: IO Middleware
-setup = do
-  let settings = defaultRequestLoggerSettings
-  mkRequestLogger settings {destination = Callback $ timestampCallback settings}
+setup = pure $ ipLogger . logStdoutDev
 
-timestampCallback :: RequestLoggerSettings -> Callback
-timestampCallback settings logStr = do
+ipLogger :: Middleware
+ipLogger app request respond = do
+  let ipAddress = show $ remoteHost request
   currentTime <- getCurrentTime
-  let timestamp = formatTime defaultTimeLocale "[%Y-%m-%d %H:%M:%S] " currentTime
-  callback $ toLogStr timestamp <> logStr
-  Control.when settings.autoFlush flush
-  where
-    (callback, flush) =
-      -- This code was copied from mkRequestLogger
-      case destination settings of
-        Handle h -> (BS.hPutStr h . fromLogStr, hFlush h)
-        Logger l -> (pushLogStr l, flushLogStr l)
-        Callback c -> (c, return ())
+  let timestamp = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" currentTime
+  putStrLn $ "[" ++ foreground Blue timestamp ++ "] IP: " ++ ipAddress
+  Control.forM_ (requestHeaders request) $ putStrLn . ("  " ++) . show
+  app request respond
+
+foreground :: Color -> String -> String
+foreground color text =
+  setSGRCode [SetColor Foreground Vivid color] ++ text ++ setSGRCode [SetDefaultColor Foreground]
