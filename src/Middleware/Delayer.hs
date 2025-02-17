c7abddccb74e5c6469bc222ced3f22b4c91e8c23
Christian Brolin <christian.brolin@carboncloud.com>
2025-02-17 10:06:27 +0100

    Delay 405, Method not allowed, as well.

--- src/Middleware/Delayer.hs
+++ src/Middleware/Delayer.hs
@@ -1,19 +1,19 @@
 module Middleware.Delayer (delayBadRequests) where
 
-import Control.Concurrent (threadDelay, myThreadId)
+import Control.Concurrent (myThreadId, threadDelay)
 import Control.Monad qualified as Control
-import Network.HTTP.Types (status401, status404, status400, status403, status429)
+import Network.HTTP.Types (status400, status401, status403, status404, status405, status429)
 import Network.Wai (Middleware, responseStatus)
-import System.Random (randomRIO)
 import System.IO (hFlush, stdout)
+import System.Random (randomRIO)
 
 delayBadRequests :: Middleware
 delayBadRequests app request respond = app request $ \response -> do
-  Control.when (responseStatus response `elem` [status400, status401, status403, status404, status429]) do
+  Control.when (responseStatus response `elem` [status400, status401, status403, status404, status405, status429]) do
     thread <- myThreadId
     putStrLn $ show thread ++ ": Delay bad request..."
     hFlush stdout
-    delay <- randomRIO (500_000, 2_000_000)  -- in microseconds
+    delay <- randomRIO (500_000, 2_000_000) -- in microseconds
     threadDelay delay
     putStrLn $ show thread ++ ": ...for " ++ show delay ++ " us."
     hFlush stdout
