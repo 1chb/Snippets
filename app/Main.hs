0b65d0a0de3927928535c1b5cebe2726c8d27c33
Christian Brolin <christian.brolin@carboncloud.com>
2025-02-22 19:33:30 +0100

    Use custom logger for request exceptions.

--- app/Main.hs
+++ app/Main.hs
@@ -2,29 +2,37 @@ module Main where
 
 import Api (api, server)
 import Config.Load qualified as Config
+import Data.Function ((&))
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
 import Database qualified
 import Middleware.Delayer (delayBadRequests)
 import Middleware.Logger qualified as Logger
-import Network.Wai.Handler.Warp qualified as Wai (defaultSettings, run, setPort)
+import Network.Wai.Handler.Warp qualified as Wai (defaultSettings, run, setOnException, setPort)
 import Network.Wai.Handler.WarpTLS (runTLS, tlsSettings)
 import Options (Options (development, port), getOptions)
-import Servant qualified (serveWithContext)
+import Servant qualified (layoutWithContext, serveWithContext)
 import Session qualified
 
 main :: IO ()
 main = do
   opts <- getOptions
   logger <- Logger.setup
-  putStrLn $ "Starting server at port " ++ show (port opts)
   sessionEnv <- Config.get
+  TIO.putStr $ Servant.layoutWithContext api (Session.authContext sessionEnv)
+  TIO.putStrLn $ "Starting server at port " <> T.show (port opts)
   let runner =
         if development opts
           then \app -> do
             print sessionEnv -- show this in development mode only.
             Wai.run (port opts) app
           else
-            let tlsConfig = tlsSettings "/etc/letsencrypt/live/wv2.hopto.org/fullchain.pem" "/etc/letsencrypt/live/wv2.hopto.org/privkey.pem"
-             in runTLS tlsConfig $ Wai.setPort (port opts) Wai.defaultSettings
+            let certFile = "/etc/letsencrypt/live/wv2.hopto.org/fullchain.pem"
+                keyFile = "/etc/letsencrypt/live/wv2.hopto.org/privkey.pem"
+             in runTLS (tlsSettings certFile keyFile) $
+                  Wai.defaultSettings
+                    & Wai.setOnException Logger.requestException
+                    & Wai.setPort (port opts)
   dbEnv <- Database.connectAndMigrate
   runner
     . delayBadRequests
--- src/Middleware/Logger.hs
+++ src/Middleware/Logger.hs
@@ -1,24 +1,52 @@
-module Middleware.Logger (setup) where
+module Middleware.Logger (setup, requestException) where
 
+import Control.Concurrent (myThreadId)
+import Control.Exception (SomeException)
 import Control.Monad qualified as Control
+import Data.ByteString.Char8 qualified as BS
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format (defaultTimeLocale, formatTime)
+import Network.Socket (SockAddr (..))
 import Network.Wai (Middleware, Request (remoteHost, requestHeaders))
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import System.Console.ANSI (Color (..), ColorIntensity (Vivid), ConsoleLayer (..), SGR (SetColor, SetDefaultColor), setSGRCode)
+import System.IO (hFlush, stdout)
 
 setup :: IO Middleware
 setup = pure $ ipLogger . logStdoutDev
 
 ipLogger :: Middleware
 ipLogger app request respond = do
-  let ipAddress = show $ remoteHost request
+  let ipAddress = T.show $ remoteHost request
   currentTime <- getCurrentTime
   let timestamp = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" currentTime
-  putStrLn $ "[" ++ foreground Blue timestamp ++ "] IP: " ++ ipAddress
-  Control.forM_ (requestHeaders request) $ putStrLn . ("  " ++) . show
+  thread <- myThreadId
+  TIO.putStrLn $ "[" <> T.pack (foreground Blue timestamp) <> "] - " <> T.show thread <> " - " <> ipAddress
+  Control.forM_ (requestHeaders request) $ TIO.putStrLn . ("  " <>) . T.show
+  hFlush stdout
   app request respond
 
 foreground :: Color -> String -> String
 foreground color text =
   setSGRCode [SetColor Foreground Vivid color] ++ text ++ setSGRCode [SetDefaultColor Foreground]
+
+requestException :: Maybe Request -> SomeException -> IO ()
+requestException mReq exception = do
+  currentTime <- getCurrentTime
+  thread <- myThreadId
+  let timestamp = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" currentTime
+      clientIP = maybe "<no request>" (showSockAddr . remoteHost) mReq
+  BS.putStrLn $
+    ("[" <> BS.pack (foreground Red timestamp))
+      <> ("] - " <> BS.pack (show thread))
+      <> (" - " <> clientIP)
+      <> (" - " <> BS.pack (show exception))
+  hFlush stdout
+
+showSockAddr :: SockAddr -> BS.ByteString
+showSockAddr = \case
+  SockAddrInet _ hostAddr -> BS.pack $ show hostAddr
+  SockAddrInet6 _ _ hostAddr6 _ -> BS.pack $ show hostAddr6
+  SockAddrUnix path -> BS.pack path
